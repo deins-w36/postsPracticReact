86787516d9d38875ac90956c3a36afab180e0e13
Denis <denbardakov@mail.ru>
2022-06-27 12:56:31 +0300

    done

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,8 +7,6 @@ import UsersList from '../users/users-list';
 import Post from '../post/post';
 
 
-
-
 class App extends Component {
 
 	constructor(props) {
@@ -18,17 +16,12 @@ class App extends Component {
 			menu : 'posts',
 			dataUsers : [],
 			dataPosts : [],
-			showMore : false
+			showMore : false,
+			isFilter : false
 		}
 		this.id = ''
 	}
 
-
-	onUpdateMenu = (str) => {
-		this.setState({
-			menu : str
-		})
-	}
 
 	componentDidMount() {
 		fetch('https://jsonplaceholder.typicode.com/posts')
@@ -39,7 +32,8 @@ class App extends Component {
 						return [item.userId, item.id, item.title, item.body]
 					})
 				})
-			});
+			})
+			.catch(e => console.log(e));
 
 		fetch('https://jsonplaceholder.typicode.com/users')
   			.then((response) => response.json())
@@ -49,9 +43,16 @@ class App extends Component {
 						return [item.id, item.name, item.email, item.phone]
 					})
 				})
-			});
+			})
+			.catch(e => console.log(e));
 	}
 
+
+	onUpdateMenu = (str) => {
+		this.setState({
+			menu : str
+		})
+	}
 
 	onShowMore = () => {
 		this.setState({
@@ -86,20 +87,32 @@ class App extends Component {
 
 	onFilterPostsToName = (id) => {
 		this.setState({
-			menu: 'posts'
+			menu: 'posts',
+			isFilter : true
 		})
+		
 		this.id = id
-
-		//Стейт в posts-list на активный фильтр
-		//стейт в app на фильтр 
-		//логика в app 
 	}
 
+	onChangeFilter = () => {
+		this.setState({
+			menu: 'posts',
+			isFilter : false
+		})
+	}
 
-
+	// qw = () => {
+	// 	if (this.state.dataPosts.length === 0) {
+	// 		this.setState({
+	// 			dataPosts : [['1','1','1','1']]
+	// 		})
+	// 	}
+	// }
 
     render() {
-		const {menu, dataUsers, dataPosts, showMore} = this.state;
+
+		const {menu, dataUsers, dataPosts, showMore, isFilter} = this.state;
+
 
 		let onePost = menu === 'post' ? dataPosts.filter(item => item[1] === this.id) : null
 		if (menu === 'post') {
@@ -107,19 +120,25 @@ class App extends Component {
 			onePost = [...onePost, q]
 		}
 
-		const visibleDataPosts = showMore ? dataPosts : dataPosts.filter(item => item[1] <= '8')
+		let visibleDataPosts
+		if (isFilter) {
+			visibleDataPosts = dataPosts
+		} else {
+			visibleDataPosts = showMore ? dataPosts : dataPosts.filter(item => item[1] <= '8')
+		}
+
+		
 		const visibleDataPostsWithDots = this.dataPostsWithDots(visibleDataPosts);
 
-		
-
-		
-
+		const filterName = isFilter ? visibleDataPostsWithDots.filter(item => item[0] === this.id) : visibleDataPostsWithDots
 
 		const postsContent = menu === 'posts' ? <PostsList 
 		onShowMore={this.onShowMore} 
-		visibleDataPostsWithDots={visibleDataPostsWithDots} 
+		visibleDataPostsWithDots={filterName} 
 		dataUsers={dataUsers} 
-		getId={this.getId}/> : null;
+		getId={this.getId}
+		isFilter={isFilter}
+		onChangeFilter={this.onChangeFilter}/> : null;
 		const usersContent = menu === 'users' ? <UsersList dataUsers={dataUsers} onFilterPostsToName={this.onFilterPostsToName}/> : null;
 		const postContent = menu === 'post' ? <Post onePost={onePost} onChangeMenuAfterPost={this.onChangeMenuAfterPost}/> : null;
 
--- src/components/posts/posts-list.js
+++ src/components/posts/posts-list.js
@@ -5,28 +5,33 @@ import './posts.scss';
 
 
 
-const PostsList = ({onShowMore, visibleDataPostsWithDots, dataUsers, getId}) => {
+const PostsList = (props) => { 
+
+    const {onShowMore, visibleDataPostsWithDots, dataUsers, getId, isFilter, onChangeFilter} = props;
+
+    const isDisplayFilter = isFilter ? {display : 'block'} : {display : 'none'}
+    const isDisplayShowMore = isFilter ? {display : 'none'} : {display : 'block'}
 
     const posts = visibleDataPostsWithDots.map(item => {
-
         const names = dataUsers.filter(elem => elem[0] === item[0])
         return (
             <PostsItem key={item[1]} title={item[2]} body={item[3]} userName={names[0][1]} getId={() => getId(item[1])}/>
         )
     })
-
     return (
         <section className="posts">
             <div className="container">
-                <div className="posts__wrapper">
+                <div onClick={onChangeFilter} className="posts__filter" style={isDisplayFilter}>Убрать фильтр</div>
+                <div className="posts__wrapper"> 
 
                     {posts}
                 
                 </div>
-                <div onClick={onShowMore} className="posts__more">Show more/less</div>
+                <div onClick={onShowMore} className="posts__more" style={isDisplayShowMore}>Show more/less</div>
             </div>
         </section>
     )
+
 }
 
 export default PostsList;
